718aefae523e8360ad28d70319a1e9be9c31b32a
Indra Agus Lesmana <indra.agus@kiranatama.com>
2016-12-30 11:28:19 +0700

    read row for cheking row with method displayInfo()

---
+++ app/src/main/java/com/example/android/pets/data/PetsDBHelper.java
@@ -0,0 +1,50 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.pets.data.PetContract.PetEntry;
+
+/**
+ * Created by indraaguslesmana on 12/30/16.
+ */
+
+public class PetsDBHelper extends SQLiteOpenHelper {
+    /**
+     * if schema change, You Must change database version
+     * */
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "pets.db";
+
+
+    public PetsDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_TABLE);
+        onCreate(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        super.onDowngrade(db, oldVersion, newVersion);
+    }
+
+    private static final String SQL_CREATE_TABLE =
+            "CREATE TABLE " + PetEntry.TABLE_NAME + " (" +
+                    PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL," +
+                    PetEntry.COLUMN_PET_BREED + " TEXT," +
+                    PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL," +
+                    PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+    private static final String SQL_DELETE_TABLE =
+            "DROP TABLE IF EXISTS " + PetEntry.TABLE_NAME;
+}
