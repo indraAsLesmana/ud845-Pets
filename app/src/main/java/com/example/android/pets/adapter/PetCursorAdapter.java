0e57e0535432896331795ecab7de206c0b41075e
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-01-05 06:32:17 +0700

    adding, listview, list_item, and adapter

---
+++ app/src/main/java/com/example/android/pets/adapter/PetCursorAdapter.java
@@ -0,0 +1,29 @@
+package com.example.android.pets.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by indraaguslesmana on 1/5/17.
+ */
+
+public class PetCursorAdapter extends CursorAdapter {
+
+
+    public PetCursorAdapter(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
