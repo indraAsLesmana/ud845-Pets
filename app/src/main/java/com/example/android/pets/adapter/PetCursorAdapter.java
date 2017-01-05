46c2e1d814ad12ea9d859533553b47e54ffd9161
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-01-05 09:24:55 +0700

    Edit cursor adapter…

--- app/src/main/java/com/example/android/pets/adapter/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/adapter/PetCursorAdapter.java
@@ -3,8 +3,13 @@ package com.example.android.pets.adapter;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.pets.R;
+import com.example.android.pets.data.PetContract.PetEntry;
 
 /**
  * Created by indraaguslesmana on 1/5/17.
@@ -13,17 +18,24 @@ import android.view.ViewGroup;
 public class PetCursorAdapter extends CursorAdapter {
 
 
-    public PetCursorAdapter(Context context, Cursor c, boolean autoRequery) {
-        super(context, c, autoRequery);
+    public PetCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        TextView nameTitle = (TextView) view.findViewById(R.id.name_tx);
+        TextView breedTitle = (TextView) view.findViewById(R.id.breed_tx);
 
+        String nameResult = cursor.getString(cursor.getColumnIndexOrThrow(PetEntry.COLUMN_PET_NAME));
+        String breedResult = cursor.getString(cursor.getColumnIndexOrThrow(PetEntry.COLUMN_PET_BREED));
+
+        nameTitle.setText(nameResult);
+        breedTitle.setText(breedResult);
     }
 }
